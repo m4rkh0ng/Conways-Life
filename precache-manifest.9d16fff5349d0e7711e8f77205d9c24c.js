6165d642fbacb6f742a55f868333b2a20b351755
Mark Hong <m4rkh0ng@gmail.com>
2018-11-01 15:54:48 -0400

    Updates

---
+++ precache-manifest.9d16fff5349d0e7711e8f77205d9c24c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "4cae4e62f78db2b84a9a",
+    "url": "/Conways-Life/static/js/main.4cae4e62.chunk.js"
+  },
+  {
+    "revision": "142698d95981b7b1f753",
+    "url": "/Conways-Life/static/js/1.142698d9.chunk.js"
+  },
+  {
+    "revision": "4cae4e62f78db2b84a9a",
+    "url": "/Conways-Life/static/css/main.ff9eaed0.chunk.css"
+  },
+  {
+    "revision": "9b4c68aa20dacbd21825e8fbbcfb123c",
+    "url": "/Conways-Life/index.html"
+  }
+];
